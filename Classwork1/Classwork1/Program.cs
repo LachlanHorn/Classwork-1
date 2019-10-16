94137dff6dc2be670deaf115c48c5202ded2a811
Lachlan Horn <K32118@eltham-college.org.uk>
2019-10-16 15:49:08 +0100

    Added Variables and name for Loop

--- Classwork1/Classwork1/Program.cs
+++ Classwork1/Classwork1/Program.cs
@@ -10,7 +10,19 @@ namespace Classwork1
     {
         static void Main(string[] args)
         {
-            //rthkrrtrtri
+        int students;
+
+        students = Convert.ToInt32(Console.ReadLine());
+
+         //declaring variables
+         int[] grades = new int[students];
+         string[] names = new string[students];
+             
+        for(int x = 0; x < students; x++)
+            {
+                Console.WriteLine("Please enter Name {0}", x);
+                names[x] = Console.ReadLine();
+            }
         }
     }
 }
